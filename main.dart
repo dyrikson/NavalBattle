6117c1812473e833f988306ef8fb95ec77cb1e63
povergo <korolev.petr2003@gmail.com>
2025-04-01 21:05:52 +0300

    Add main

---
+++ main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:nv/logic.dart';
+import 'package:nv/FrontEnd/ShipPlacementBackGround';
+
+void main() => runApp(const ShipPlacementBackGround());
