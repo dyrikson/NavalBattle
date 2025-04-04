5bfe0d41a17b38f71c1d459bdf6c6e3e41cd6552
Petr <121057719+dyrikson@users.noreply.github.com>
2025-04-04 18:56:50 +0300

    Update GameScreen.dart

--- FrontEnd/GameScreen.dart
+++ FrontEnd/GameScreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:nv/Backend/game.dart';
+import 'package:nv/BackEnd/game.dart';
 
 class GameBackGround extends StatelessWidget {
   const GameBackGround({super.key});
