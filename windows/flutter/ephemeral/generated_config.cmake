f5983ddc72f7442ebc533e70e6ffdea9167defd3
Petr <121057719+dyrikson@users.noreply.github.com>
2025-04-04 19:04:49 +0300

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Pitek\\NavalBattelV2\\NavalBattleV2-main" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\src\\flutter"
+  "PROJECT_DIR=D:\\Pitek\\NavalBattelV2\\NavalBattleV2-main"
+  "FLUTTER_ROOT=D:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Pitek\\NavalBattelV2\\NavalBattleV2-main\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Pitek\\NavalBattelV2\\NavalBattleV2-main"
+  "FLUTTER_TARGET=D:\\Pitek\\NavalBattelV2\\NavalBattleV2-main\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Pitek\\NavalBattelV2\\NavalBattleV2-main\\.dart_tool\\package_config.json"
+)
