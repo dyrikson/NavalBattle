de3fd884481c6bffca0e0e221ff5110e12f5043a
Petr <121057719+dyrikson@users.noreply.github.com>
2025-04-04 18:56:39 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:nv/logic.dart';
 import 'package:nv/FrontEnd/ShipPlacementBackGround';
 
 void main() => runApp(const ShipPlacementBackGround());
